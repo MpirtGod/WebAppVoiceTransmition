94017370895d829632afe112fb5265df0b8f819c
MpirtGod <AVMelnikov@urfu.me>
2023-04-13 17:54:10 +0500

    Timer

--- templates/js/voice_record.js
+++ templates/js/voice_record.js
@@ -1,26 +1,58 @@
 const recordButton = document.getElementById('recordButton');
-      let mediaRecorder;
-      let chunks = [];
+let mediaRecorder;
+let chunks = [];
+var class_timer = document.getElementById('timer');
+var sec = 0;
+var min = 0;
+var hrs = 0;
+var t;
 
-      navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(stream => {
-          mediaRecorder = new MediaRecorder(stream);
+function tick(){
+    sec++;
+    if (sec >= 60) {
+        sec = 0;
+        min++;
+        if (min >= 60) {
+            min = 0;
+            hrs++;
+        }
+    }
+}
 
-          recordButton.addEventListener('click', () => {
+function add() {
+    tick();
+    class_timer.textContent = (hrs > 9 ? hrs : "0" + hrs)
+        	 + ":" + (min > 9 ? min : "0" + min)
+       		 + ":" + (sec > 9 ? sec : "0" + sec);
+    timer();
+}
+
+function timer() {
+    t = setTimeout(add, 1000);
+}
+
+navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+        mediaRecorder = new MediaRecorder(stream);
+        recordButton.addEventListener('click', () => {
             if (mediaRecorder.state === 'inactive') {
-              mediaRecorder.start();
-              recordButton.textContent = 'Остановить запись ⏹️';
+                timer();
+                mediaRecorder.start();
+                recordButton.textContent = 'Остановить запись ⏹️';
             } else {
-              mediaRecorder.stop();
-              recordButton.textContent = 'Записать голос ▶️';
+                mediaRecorder.stop();
+                clearTimeout(t);
+                class_timer.textContent = "00:00:00";
+                seconds = 0; minutes = 0; hours = 0;
+                recordButton.textContent = 'Записать голос ▶️';
             }
-          });
+        });
 
-          mediaRecorder.addEventListener('dataavailable', event => {
+        mediaRecorder.addEventListener('dataavailable', event => {
             chunks.push(event.data);
-          });
+        });
 
-          mediaRecorder.addEventListener('stop', () => {
+        mediaRecorder.addEventListener('stop', () => {
             const blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' });
             const url = URL.createObjectURL(blob);
             const a = document.createElement('a');
@@ -32,5 +64,5 @@ const recordButton = document.getElementById('recordButton');
           });
         })
         .catch(error => {
-          console.error(error);
+            console.error(error);
         });

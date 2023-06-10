f18e904e4b999e5fa4da885b3a57f17b0a95a04c
MpirtGod <AVMelnikov@urfu.me>
2023-06-10 20:27:06 +0500

    Контакты

--- app.py
+++ app.py
@@ -33,6 +33,9 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html")
 
 @app.route("/js/<path:path>")
 def send_js(path):
@@ -75,4 +78,4 @@ def upload_audio():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
